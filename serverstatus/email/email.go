42365acfe79a4e4a621eccb4abf31065be9a3691
genchadt <62498018+genchadt@users.noreply.github.com>
2024-09-25 21:53:32 -0400

    Implement GetLastRebootTime

--- serverstatus/email/email.go
+++ serverstatus/email/email.go
@@ -1,16 +1,17 @@
 package email
 
 type EmailData struct {
-    ServerHostname    string
-    ServerTime        string
-    ServerUptime      string
-    PackageUpdates    string
-    DiskDetails       string
-    MemoryDetails     string
-    CPULoadDetails    string
-    ActiveSSH         string
-    PreviousSSH       string
-    NetworkDetails    string
-    CrowdSecAlerts    string
-    CrowdSecDecisions string
+	ServerHostname    string
+	ServerTime        string
+	ServerUptime      string
+	LastRebootTime    string
+	PackageUpdates    string
+	DiskDetails       string
+	MemoryDetails     string
+	CPULoadDetails    string
+	ActiveSSH         string
+	PreviousSSH       string
+	NetworkDetails    string
+	CrowdSecAlerts    string
+	CrowdSecDecisions string
 }
--- serverstatus/main.go
+++ serverstatus/main.go
@@ -1,55 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"os"
+	"time"
 
-    "serverstatus/email"
-    "serverstatus/metrics"
+	"serverstatus/email"
+	"serverstatus/metrics"
 )
 
 func main() {
-    // Server details here
-    serverHostname := "Lightsail Web"
-    serverTime := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
-    serverUptime := metrics.GetUptime()
+	// Server details here
+	serverHostname := "Lightsail Web"
+	serverTime := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
+	serverUptime := metrics.GetUptime()
+	lastRebootTime := metrics.GetLastRebootTime()
 
-    // Metrics collection
-    packageUpdates := metrics.GetPackageUpdates()
-    diskDetails := metrics.GetDiskDetails()
-    cpuLoadDetails := metrics.GetCPULoadDetails()
-    memoryDetails := metrics.GetMemoryDetails()
-    activeSSH := metrics.GetActiveSSHSessions()
-    previousSSH := metrics.GetPreviousSSHSessions()
-    networkDetails := metrics.GetNetworkDetails()
-    crowdSecAlerts := metrics.GetCrowdSecAlerts()
-    crowdSecDecisions := metrics.GetCrowdSecDecisions()
+	// Metrics collection
+	packageUpdates := metrics.GetPackageUpdates()
+	diskDetails := metrics.GetDiskDetails()
+	cpuLoadDetails := metrics.GetCPULoadDetails()
+	memoryDetails := metrics.GetMemoryDetails()
+	activeSSH := metrics.GetActiveSSHSessions()
+	previousSSH := metrics.GetPreviousSSHSessions()
+	networkDetails := metrics.GetNetworkDetails()
+	crowdSecAlerts := metrics.GetCrowdSecAlerts()
+	crowdSecDecisions := metrics.GetCrowdSecDecisions()
 
-    // Construct Email Body
-    emailBody := email.ConstructEmailBody(email.EmailData{
-        ServerHostname:  serverHostname,
-        ServerTime:      serverTime,
-        ServerUptime:    serverUptime,
-        PackageUpdates:  packageUpdates,
-        DiskDetails:     diskDetails,
-        MemoryDetails:   memoryDetails,
-        CPULoadDetails:  cpuLoadDetails,
-        ActiveSSH:       activeSSH,
-        PreviousSSH:     previousSSH,
-        NetworkDetails:  networkDetails,
-        CrowdSecAlerts:  crowdSecAlerts,
-        CrowdSecDecisions: crowdSecDecisions,
-    })
+	// Construct Email Body
+	emailBody := email.ConstructEmailBody(email.EmailData{
+		ServerHostname:    serverHostname,
+		ServerTime:        serverTime,
+		ServerUptime:      serverUptime,
+		LastRebootTime:    lastRebootTime,
+		PackageUpdates:    packageUpdates,
+		DiskDetails:       diskDetails,
+		MemoryDetails:     memoryDetails,
+		CPULoadDetails:    cpuLoadDetails,
+		ActiveSSH:         activeSSH,
+		PreviousSSH:       previousSSH,
+		NetworkDetails:    networkDetails,
+		CrowdSecAlerts:    crowdSecAlerts,
+		CrowdSecDecisions: crowdSecDecisions,
+	})
 
-    // Send Email
-    today := time.Now().Format("2006-01-02")
-    subject := fmt.Sprintf("Daily System Report, %s", today)
-    err := email.SendEmail(subject, emailBody, "webmaster@timothywb.com")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to send email: %v\n", err)
-        os.Exit(1)
-    }
+	// Send Email
+	today := time.Now().Format("2006-01-02")
+	subject := fmt.Sprintf("Daily System Report, %s", today)
+	err := email.SendEmail(subject, emailBody, "webmaster@timothywb.com")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to send email: %v\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Println("Email sent successfully.")
+	fmt.Println("Email sent successfully.")
 }

e670b63b389cdd1f6bd39a83b45e7be2e071c80c
genchadt <62498018+genchadt@users.noreply.github.com>
2024-09-25 21:48:57 -0400

    New function returns time of last reboot

--- serverstatus/metrics/uptime.go
+++ serverstatus/metrics/uptime.go
@@ -1,18 +1,29 @@
 package metrics
 
 import (
-    "bytes"
-    "os/exec"
-    "strings"
+	"bytes"
+	"os/exec"
+	"strings"
 )
 
 func GetUptime() string {
-    cmd := exec.Command("uptime", "-p")
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
-    if err != nil {
-        return "Unable to retrieve uptime"
-    }
-    return strings.TrimSpace(out.String())
+	cmd := exec.Command("uptime", "-p")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "Unable to retrieve uptime"
+	}
+	return strings.TrimSpace(out.String())
+}
+
+func GetLastRebootTime() string {
+	cmd := exec.Command("uptime", "-s")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "Unable to retrieve last reboot time"
+	}
+	return strings.TrimSpace(out.String())
 }

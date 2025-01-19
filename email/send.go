70486c6329e3885363946eee3411745055bb1fee
genchadt <62498018+genchadt@users.noreply.github.com>
2025-01-18 21:45:29 -0500

    refactor email sending logic to use configuration for SMTP credentials and establish TLS connection

--- email/send.go
+++ email/send.go
@@ -1,52 +1,62 @@
 package email
 
 import (
+	"crypto/tls"
 	"fmt"
-	"net/smtp"
-	"os"
-	// "github.com/wneessen/go-mail"
+	"strconv"
+
+	"serverstatus/config"
+
+	"github.com/wneessen/go-mail"
 )
 
 // SendEmail sends an email with the given subject and body to the recipient at the given
 // address. It uses the SMTP server and credentials specified in the function body.
 // Note that the auth information should be replaced with a secure storage mechanism.
 func SendEmail(subject, body, to string) error {
-	// Get credentials from environment variables
-	smtpHost := os.Getenv("SMTP_HOST")
-	smtpPort := os.Getenv("SMTP_PORT")
-	if smtpHost == "" || smtpPort == "" {
-		return fmt.Errorf("SMTP_HOST and SMTP_PORT environment variables must be set")
-	}
-
-	smtpUser := os.Getenv("SMTP_USER")
-	smtpPass := os.Getenv("SMTP_PASS")
-	if smtpUser == "" || smtpPass == "" {
-		return fmt.Errorf("SMTP_USER and SMTP_PASS environment variables must be set")
-	}
-
-	fromEmail := os.Getenv("FROM_EMAIL")
-
-	if fromEmail == "" {
-		return fmt.Errorf("FROM_EMAIL environment variable must be set")
-	}
-
-	auth := smtp.PlainAuth("", smtpUser, smtpPass, smtpHost)
-
-	from := fmt.Sprintf("Lightsail Web Updates <%s>", fromEmail)
-
-	msg := "MIME-Version: 1.0\r\n"
-	msg += "Content-Type: text/html; charset=\"UTF-8\"\r\n"
-	msg += fmt.Sprintf("From: %s\r\n", from)
-	msg += fmt.Sprintf("To: %s\r\n", to)
-	msg += fmt.Sprintf("Subject: %s\r\n\r\n", subject)
-	msg += body
-
-	smtpAddr := fmt.Sprintf("%s:%s", smtpHost, smtpPort)
-
-	// SMTP info goes here
-	err := smtp.SendMail(smtpAddr, auth, fromEmail, []string{to}, []byte(msg))
+	// Get credentials from environment variables or secure storage
+	cfg, err := config.LoadConfig()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to load config: %v", err)
 	}
+
+	// Convert SMTP port to integer
+	smtpPortInt, err := strconv.Atoi(cfg.SmtpPort)
+	if err != nil {
+		return fmt.Errorf("invalid SMTP_PORT: %v", err)
+	}
+
+	// Use go-mail library with TLS
+	m := mail.NewMsg()
+	if err := m.FromFormat(cfg.FromName, cfg.FromEmail); err != nil {
+		return fmt.Errorf("failed to set sender: %v", err)
+	}
+	if err := m.To(to); err != nil {
+		return fmt.Errorf("failed to set recipient: %v", err)
+	}
+
+	m.Subject(subject)
+	m.SetBodyString(mail.TypeTextHTML, body)
+
+	// Establish a TLS connection
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         cfg.SmtpHost,
+	}
+
+	c, err := mail.NewClient(cfg.SmtpHost,
+		mail.WithPort(smtpPortInt),
+		mail.WithUsername(cfg.SmtpUser),
+		mail.WithPassword(cfg.SmtpPass),
+		mail.WithTLSConfig(tlsConfig),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create mail client: %v", err)
+	}
+
+	if err := c.DialAndSend(m); err != nil {
+		return fmt.Errorf("failed to send email: %v", err)
+	}
+
 	return nil
 }

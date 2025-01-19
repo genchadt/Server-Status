d8cc648837736534bfad90381c8609faab5f147c
genchadt <62498018+genchadt@users.noreply.github.com>
2025-01-18 20:21:25 -0500

    refactor SanitizeIPAddress function to improve IP sanitization logic

--- utils/utils.go
+++ utils/utils.go
@@ -6,8 +6,6 @@ import "strings"
 // SanitizeIPAddress takes an IP address as input and removes any "Ip:" prefix,
 // replacing all dots with hyphens to sanitize the IP address format.
 func SanitizeIPAddress(ip string) string {
-	// Raw CrowdSec decisions include IPs with "Ip:" prefix
-	ip = strings.TrimPrefix(ip, "Ip:")
-
-	return strings.ReplaceAll(ip, ".", "-")
+	ip = strings.ReplaceAll(ip, ".", "-")
+	return strings.TrimPrefix(ip, "Ip:")
 }
